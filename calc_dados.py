ead86796e08c3faeccd4b9d798ec2dcc0c5206b3
JOÃO VICTOR ZUCCARATTO <47816949855@info.local>
2026-03-06 19:41:29 -0300

    Master

--- calc_dados.py
+++ calc_dados.py
@@ -22,3 +22,8 @@ def produto_escalar(v1, v2):
 def mae(v1, v2):
     validar_vetores(v1, v2)
     return sum(abs(a - b) for a, b in zip(v1, v2)) / len(v1)
+
+
+def mse(v1, v2):
+    validar_vetores(v1, v2)
+    return sum((a - b ) ** 2 for a, b in zip (v1, v2)) / len(v1)
